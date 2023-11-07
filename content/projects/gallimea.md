3b2a380da3ea842cd1ed2a409279d0f3a4a5701a
Nicolas Crausaz <n.crausaz99@gmail.com>
2023-11-07 16:18:17 +0100

    chore: reworked nextversion with hugo

---
+++ content/projects/gallimea.md
@@ -0,0 +1,13 @@
++++
+author = "Nicolas Crausaz"
+title = "Gallimea"
+date = "2023-11-07"
+projectType = "Freelance"
+description = '''
+Gallimea est une application de comptabilité destinées aux petites entreprises, indépendants, clubs et associations.
+'''
+techs = ["PHP", "JavaScript"]
+Image = "gallimea.png"
++++
+
+Gallimea est un application de comptabilité.
---
+++ themes/ncrausaz/assets/css/tailwind.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./themes/**/layouts/**/*.html",
+    "./content/**/layouts/**/*.html",
+    "./layouts/**/*.html",
+    "./content/**/*.html"
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      animation: {
+        text: 'text 5s ease infinite',
+      },
+      keyframes: {
+        text: {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
+        },
+      },
+      colors: {
+        deepblue: '#001521',
+        deepbluegrad: '#001530'
+      },
+    },
+  },
+  plugins: [
+    // require('@tailwindcss/aspect-ratio'),
+    // require('@tailwindcss/forms'),
+    // require('@tailwindcss/typography'),
+  ]
+}
