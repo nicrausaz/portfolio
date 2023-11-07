3eb0a88d8d07af4756631917cfdb3b030eb026eb
Nicolas Crausaz <n.crausaz99@gmail.com>
2023-11-07 18:05:08 +0100

    feat: improved navbar and added a few projects

---
+++ static/js/main.js
@@ -0,0 +1,24 @@
+const sections = document.querySelectorAll('.section')
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.5,
+}
+
+const callback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (!entry.target.id) return
+
+    const menuLink = document.querySelector(`[data-section=${entry.target.id}]`)
+    if (entry.isIntersecting) {
+      menuLink.classList.add('navbar-active')
+    } else {
+      menuLink.classList.remove('navbar-active')
+    }
+  })
+}
+
+const observer = new IntersectionObserver(callback, options)
+sections.forEach((section) => {
+  observer.observe(section)
+})
