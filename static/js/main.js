6df820049f0af315e072ed3419ee71a74376b597
Nicolas Crausaz <n.crausaz99@gmail.com>
2023-11-08 12:06:44 +0100

    feat: animations & fonts

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,4 @@
+// Observers for every sections that update the navbar links when the section is in the viewport
 const sections = document.querySelectorAll('.section')
 const options = {
   root: null,
@@ -7,10 +8,23 @@ const options = {
 
 const callback = (entries, observer) => {
   entries.forEach((entry) => {
-    if (!entry.target.id) return
+    if (!entry.target.id) {
+      return
+    }
 
     const menuLink = document.querySelector(`[data-section=${entry.target.id}]`)
     if (entry.isIntersecting) {
+      anime({
+        targets: '#nav-content',
+        opacity: 1,
+        duration: 1000,
+        easing: 'easeOutExpo',
+      })
+      anime({
+        targets: `#${entry.target.id}`,
+        opacity: 1,
+        easing: 'easeInOutSine',
+      })
       menuLink.classList.add('navbar-active')
     } else {
       menuLink.classList.remove('navbar-active')
@@ -21,4 +35,21 @@ const callback = (entries, observer) => {
 const observer = new IntersectionObserver(callback, options)
 sections.forEach((section) => {
   observer.observe(section)
+})
+
+// Animations
+anime({
+  targets: '#main-title',
+  opacity: 1,
+  duration: 2000,
+  delay: 1000,
+  easing: 'easeOutExpo',
+})
+
+anime({
+  targets: '#call-to-scroll',
+  opacity: 1,
+  duration: 1000,
+  delay: 4000,
+  easing: 'easeOutExpo',
 })
